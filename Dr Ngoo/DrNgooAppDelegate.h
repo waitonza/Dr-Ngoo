4ee3887602a434adb2a13d408cd174e81366b7ea
waitonza <wairung3421@gmail.com>
2012-03-07 23:25:27 +0700

    First Project
    Dr Ngoo First Project for iOS5

---
+++ Dr Ngoo/DrNgooAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DrNgooAppDelegate.h
+//  Dr Ngoo
+//
+//  Created by Wairung Tiranalinvit on 3/7/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrNgooAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dr Ngoo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dr Ngoo
+//
+//  Created by Wairung Tiranalinvit on 3/7/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DrNgooAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DrNgooAppDelegate class]));
+    }
+}
