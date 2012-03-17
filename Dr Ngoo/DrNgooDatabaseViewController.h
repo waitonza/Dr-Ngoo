0381ba1d4dc66c3da5c29d6c2bb9f045d11cbdb0
waitonza <wairung3421@gmail.com>
2012-03-17 10:16:23 +0700

    little

--- Dr Ngoo/DrNgooDatabaseViewController.h
+++ Dr Ngoo/DrNgooDatabaseViewController.h
@@ -2,12 +2,20 @@
 //  DrNgooDatabaseViewController.h
 //  Dr Ngoo
 //
-//  Created by Wairung Tiranalinvit on 3/16/55 BE.
+//  Created by Wairung Tiranalinvit on 3/14/55 BE.
 //  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface DrNgooDatabaseViewController : UIViewController
+@interface DrNgooDatabaseViewController : UIViewController 
+<UITableViewDataSource,UITableViewDelegate,UISearchDisplayDelegate,UISearchBarDelegate>
+
+@property (strong, nonatomic) IBOutlet UITableView *table;
+@property (strong, nonatomic) IBOutlet UISearchBar *search;
+@property (strong, nonatomic) NSArray *snakes;
+@property (strong, nonatomic) NSMutableArray *filteredListContent;
+@property (nonatomic, copy) NSString *savedSearchTerm;
+@property (assign, nonatomic) BOOL searchWasActive;
 
 @end
